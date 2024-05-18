b9126dfc73b460e040d83b45e2995723a034cfe6
Your Name <Ljcomp@xyz.com>
2024-05-18 10:21:35 +0530

    Commit

--- src/ThisKeyword.java
+++ src/ThisKeyword.java
@@ -1,10 +1,18 @@
 public class ThisKeyword {
     
+    int id;
+    String name;
     void show() {
         System.out.println(this);
     }
+    ThisKeyword(int id, String name) {
+        this();
+        this.id = id;
+        this.name = name;
+    } 
+    ThisKeyword() { System.out.println("In Simple Constructor");}
     public static void main(String[] args) {
-        ThisKeyword obj = new ThisKeyword();
+        ThisKeyword obj = new ThisKeyword(1, "Vraj");
         System.out.println(obj);
         obj.show();
     }
